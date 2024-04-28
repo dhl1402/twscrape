ba7dcb40acad0836210774304b4c34b4d0a93605
DHL <dhl1402@gmail.com>
2024-04-29 01:52:09 +0700

    Increase lock time to 5 mins

--- twscrape/queue_client.py
+++ twscrape/queue_client.py
@@ -96,7 +96,7 @@ class QueueClient:
             await self.pool.lock_until(ctx.acc.username, self.queue, reset_at, ctx.req_count)
             return
 
-        await self.pool.lock_until(ctx.acc.username, self.queue, utc.ts() + 60 * 2, ctx.req_count)
+        await self.pool.lock_until(ctx.acc.username, self.queue, utc.ts() + 60 * 5, ctx.req_count)
 
     async def _get_ctx(self):
         if self.ctx:
