ed6d59426303dac4a64d1079742064c13b9ce3d6
DHL <dhl1402@gmail.com>
2024-05-01 07:50:00 +0700

    Update limit_remaining

--- twscrape/queue_client.py
+++ twscrape/queue_client.py
@@ -146,7 +146,7 @@ class QueueClient:
             exit(1)
 
         # general api rate limit
-        if limit_remaining < 48 and limit_reset > 0:
+        if limit_remaining < 47 and limit_reset > 0:
             logger.info(f"Rate limited: {log_msg}")
             await self._close_ctx(limit_reset)
             raise HandledError()
